3a3f261d86d1a97c20c292eeff9e499b5fd4b25f
HanshuYAN <hanshuyan2017@outlook.com>
2021-10-12 11:01:17 +0800

    ...

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Resnet 18
-CUDA_VISIBLE_DEVICES=0 python CIFAR10_Res18_CIFS.py --is_Train --GPU_IDs 0 --network _  --exp_path ./experiments/Cln_Res18_cifar10 --attack_loss CE
-CUDA_VISIBLE_DEVICES=1 python CIFAR10_Res18_CIFS.py --is_Train --is_AdvTr --GPU_IDs 0 --network _  --exp_path ./experiments/PAT_Res18_cifar10 --attack_loss CE
+CUDA_VISIBLE_DEVICES=0 python CIFAR10_Res18_CIFS.py --is_Train --GPU_IDs 0 --network _  --exp_path ./experiments/... --attack_loss CE
+CUDA_VISIBLE_DEVICES=1 python CIFAR10_Res18_CIFS.py --is_Train --is_AdvTr --GPU_IDs 0 --network _  --exp_path ./experiments/... --attack_loss CE
 CUDA_VISIBLE_DEVICES=0 python CIFAR10_Res18_CIFS_test.py --GPU_IDs 0 --network _  --which_model ./experiments/...
 
 # CAS (Layer4)
@@ -10,11 +10,9 @@ CUDA_VISIBLE_DEVICES=0 python CIFAR10_Res18_CIFS.py --is_Train --is_AdvTr --GPU_
 CUDA_VISIBLE_DEVICES=0 python CIFAR10_Res18_CIFS_test.py --GPU_IDs 0 --network CAS_L4 --which_model ./experiments/...pt --is_attack 1 --is_joint 1 --beta_atk 2
 
 #  CIFS
-CUDA_VISIBLE_DEVICES=1 python CIFAR10_Res18_CIFS.py --is_Train --GPU_IDs 0 --network CIFS_L4 --exp_path ./experiments/Cln_Res18_cifar10_CIFS
-CUDA_VISIBLE_DEVICES=0 python CIFAR10_Res18_CIFS.py --is_Train --is_AdvTr --GPU_IDs 0 --network CIFS_L4 --exp_path ./experiments/PAT_Res18_cifar10_CIFS --attack_loss Joint --beta_atk 2 \
-                                                                                        --resume --checkpoint ./experiments/Cln_Res18_cifar10_CIFS/nets/ckp_best.pt
+CUDA_VISIBLE_DEVICES=1 python CIFAR10_Res18_CIFS.py --is_Train --GPU_IDs 0 --network CIFS_L4 --exp_path ./experiments/...
+CUDA_VISIBLE_DEVICES=0 python CIFAR10_Res18_CIFS.py --is_Train --is_AdvTr --GPU_IDs 0 --network CIFS_L4 --exp_path ./experiments/... --attack_loss Joint --beta_atk 2
 
-# CUDA_VISIBLE_DEVICES=0 python CIFAR10_Res18_CIFS_test.py --GPU_IDs 0 --network CSAFR_topk --which_model ./experiments/....pt --is_attack 1 --is_joint 1 --beta_atk 2
 
 
 
